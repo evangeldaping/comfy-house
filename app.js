e6abd378ac16c51b2a876ddde518546c94b8ae85
evangel <evangeldaping@gmail.com>
2020-11-26 18:27:13 -0800

    class UI

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cartOverlay = document.querySelector('.cart-overlay');
 const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
-const productsDOM= document.querySelector('.products-center');
+const productsDOM = document.querySelector('.products-center');
 
 //cart
 let cart = [];
@@ -37,7 +37,30 @@ class Products {
 
 // display products
 class UI {
-
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <!-- single product -->
+                <article class="product">
+                    <div class="img-container">
+                        <img src=${product.image}
+                            alt="product" 
+                            class="product-img"
+                        >
+                        <button class="bag-btn" data-d=${product.id}>
+                            <i class="fas fa-shopping-cart"></i>
+                            add to bag
+                        </button>
+                    </div>
+                    <h3>${product.title}</h3>
+                    <h4>${product.price}</h4>
+                </article>
+            <!-- end of single product -->
+            `
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 // local storage
@@ -50,5 +73,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const products = new Products();
 
     // get all products
-    products.getProducts().then(products => console.log(products));
-})
+    products.getProducts().then(products => 
+        ui.displayProducts(products))
+});
